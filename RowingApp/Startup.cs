09de2dbc7ce507927a26ec9c9147e4cc8b43993e
ruth-mcelroy <ruth.mcelroy89@gmail.com>
2017-01-16 19:20:08 +0000

    Initial setup

---
+++ RowingApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RowingApp.Startup))]
+namespace RowingApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
